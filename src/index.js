b6151bbb5b4880a7bfeff4a33aca9fe672832dbe
Aman Kumar <aman01197kumar@gmail.com>
2023-08-20 02:31:21 -0700

    fixed: redux persist

--- src/index.js
+++ src/index.js
@@ -6,23 +6,19 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import persistStore from "redux-persist/es/persistStore";
+// import persistStore from "redux-persist/es/persistStore";
 import { PersistGate } from "redux-persist/integration/react";
+// import persistStore from "redux-persist/es/persistStore";
 import persistStore from "redux-persist/es/persistStore";
-<<<<<<<<< Temporary merge branch 1
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const peristor = persistStore(store)
 root.render(
   <Provider store={store}>
-<<<<<<<<< Temporary merge branch 1
-    <PersistGate persistor={peristor  }>
-=========
+
     <PersistGate persistor={peristor}>
 
->>>>>>>>> Temporary merge branch 2
-    <App />
-      </PersistGate>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
