5e06101ed593d63aa34d2ac830c90f42b36730d8
Aman Kumar <aman01197kumar@gmail.com>
2023-08-25 18:21:52 +0530

    uninstall some useless packeages

--- src/components/Utilites/Footer/Footer.jsx
+++ src/components/Utilites/Footer/Footer.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Button from "../../Button/Button";
 import AmazonBrief from "../AmazonBrief/AmazonBrief";
 import AmazonServices from "../AmazonServices/AmazonServices";
-import MidFooter from "../MidFooter/MidFooter";
 
 function Footer({ title }) {
   return (
     <div>
       <Button title={title} />
       <AmazonBrief />
-      <MidFooter />
       <AmazonServices />
     </div>
   );
